0c6af3702c2c7c8cbd8b48e905e2e3fc5887159a
Herman Soto <hermansoto@me.com>
2018-11-28 16:06:14 -0500

    Add a User.findOne to read_test.

--- test/reading_test.js
+++ test/reading_test.js
@@ -6,18 +6,26 @@ describe('Reading users out of database', () => {
 
   beforeEach((done) => {
     herman = new User({
-      firstName: 'Herman'
+      firstName: 'herman'
     });
     herman.save()
       .then(() => done());
   }); 
 
   it('finds all users with the name of herman', (done) => {
-    User.find({firstName: 'Herman'})
+    User.find({firstName: 'herman'})
       .then((users) => {
         console.log(users[0]._id); // need to be converted toString
         console.log(herman._id); // need to be converted toString
         assert(users[0]._id.toString() === herman._id.toString());
+        done();
+      });
+  });
+
+  it('finds a user with an id', (done) => {
+    User.findOne({ _id: herman._id })
+      .then((user) => {
+        assert(user.firstName === 'herman');
         done();
       });
   });
