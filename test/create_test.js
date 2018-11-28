3b0fd19143314e6ec349f8de3f80446c0533d6fe
Herman Soto <hermansoto@me.com>
2018-11-28 12:24:26 -0500

    Add assert to check if herman is not a new user(meaning saved to db). Add a done callback to wait to check to see if user has been saved before running test.

--- test/create_test.js
+++ test/create_test.js
@@ -3,13 +3,18 @@ const assert = require('assert');
 // add User model
 const User = require('../models/user');
 
-describe('Creating Records', () => {
+describe('Creating Records', (done) => {
   it('saves a user', () => {
     // creates an instance of User to a const named herman
-    const dorota = new User({
-      firstName: "dorota"
+    const herman = new User({
+      firstName: "herman"
     });
     // saves herman to database
-    dorota.save();
+    herman.save()
+      .then(() => {
+        // Has herman been saved properly?
+        assert(!herman.isNew);
+        done();
+      })
   });
 });
